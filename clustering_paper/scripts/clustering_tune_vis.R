e1333c32bf4ef8f387eebc012b6af80b01b360bd
TengMCing <llreczx@gmail.com>
2020-12-16 03:24:33 +1100

    para choice

--- clustering_paper/scripts/clustering_tune_vis.R
+++ clustering_paper/scripts/clustering_tune_vis.R
@@ -1,25 +1,26 @@
 library(tidyverse)
 
 grid <- read_csv("data/clustering_grid.csv")
+grid <- rename(grid, AdjDist = adj_dist, ActiveTime = active_time)
 
 p1 <- ggplot(filter(grid)) +
-  geom_point(aes(adj_dist, count)) +
-  geom_line(aes(adj_dist, count, group = active_time, col = factor(active_time))) +
+  geom_point(aes(AdjDist, count)) +
+  geom_line(aes(AdjDist, count, group = ActiveTime, col = factor(ActiveTime))) +
   scale_x_continuous(breaks = seq(1000,10000,1000)) +
-  labs(color = "active_time") +
+  labs(color = "ActiveTime") +
   ylab("Number of clusters") +
-  ggtitle('Number of clusters under different settings of "active_time" and "adj_dist"')
+  theme_bw(base_size = 20)
 
 
 ggsave("figures/clustering_tuning_1.jpeg", p1, width = 10, dpi = 600)
 
-p2 <- ggplot(filter(grid, adj_dist>2000)) +
-  geom_point(aes(active_time, count)) +
-  geom_line(aes(active_time, count, group = adj_dist, col = factor(adj_dist))) +
+p2 <- ggplot(filter(grid, AdjDist>2000)) +
+  geom_point(aes(ActiveTime, count)) +
+  geom_line(aes(ActiveTime, count, group = AdjDist, col = factor(AdjDist))) +
   scale_x_continuous(breaks = seq(3,48,3)) +
-  labs(color = "adj_dist") +
+  labs(color = "AdjDist") +
   ylab("Number of clusters") +
-  ggtitle('Number of clusters under different settings of "active_time" and "adj_dist"')
+  theme_bw(base_size = 20)
 
 
 ggsave("figures/clustering_tuning_2.jpeg", p2, width = 10, dpi = 600)
